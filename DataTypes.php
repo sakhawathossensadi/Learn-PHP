578f5e268f83272f3be7efe73596edd600c84b51
sakhawathossensadi <newtonss919@gmail.com>
2022-09-29 01:42:00 +0600

    PHP data types

---
+++ DataTypes.php
@@ -0,0 +1,20 @@
+<?php
+
+$str = "Hello World"; // String
+var_dump($str);
+echo "<br>";
+$val1 = 12; //Integer
+var_dump($val1);
+echo "<br>";
+$val2 = 22.57; //Float
+var_dump($val2);
+echo "<br>";
+$x = true; //Boolean
+var_dump($x);
+echo "<br>";
+$arr = array(1, 2, 3); // Array
+var_dump($arr);
+echo "<br>";
+//$obj = new MyClass(); //Object
+$a1 = null; //Null
+var_dump($a1);
