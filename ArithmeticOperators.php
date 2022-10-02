dfb8810ec1b04333327ae74fef6ffa540087c6f2
sakhawathossensadi <newtonss919@gmail.com>
2022-10-02 16:49:49 +0600

    Arithmetic operators

---
+++ ArithmeticOperators.php
@@ -0,0 +1,42 @@
+<?php
+
+$a = 20;
+$b = 10;
+
+$c = $a + $b;
+echo $c . "<br>";
+
+$c = $a - $b;
+echo $c . "<br>";
+
+$c = $a * $b;
+echo $c . "<br>";
+
+$c = $a / $b;
+echo $c . "<br>";
+
+$c = 14 % 3;
+echo $c . "<br>";
+
+$c = 2 ** 5;
+echo $c . "<br>";
+
+$inc = 10;
+
+echo $inc++ . "<br>";
+echo $inc . "<br>";
+
+$inc1 = 10;
+
+echo ++$inc1 . "<br>";
+echo $inc1 . "<br>";
+
+$dec = 10;
+
+echo $dec-- . "<br>";
+echo $dec . "<br>";
+
+$dec1 = 10;
+
+echo --$dec1 . "<br>";
+echo $dec1 . "<br>";
