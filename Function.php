3363fd294a1687de8e3db1a7376caccc21ab659c
sakhawathossensadi <newtonss919@gmail.com>
2022-10-04 01:50:27 +0600

    PHP function

---
+++ Function.php
@@ -0,0 +1,21 @@
+<?php
+
+//function definition
+function echoFunction()
+{
+    echo "Hello World!" . "<br>";
+}
+
+//calling a function
+echoFunction();
+
+//function definition
+function sum()
+{
+    $a = 10;
+    $b = 20;
+    echo "Result : " . ($a + $b) . "<br>";
+}
+
+//calling a function
+sum();
