008add0c33e79ed10a9a56e0c248b7f414c277a1
sakhawathossensadi <newtonss919@gmail.com>
2022-10-04 01:16:31 +0600

    PHP ternary operator

---
+++ TernaryOperator.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 10;
+
+//Ternary Operator
+($a > 15) ? $z = "True" : $z = "False";
+
+echo $z . "<br>";
+
+$a = 20;
+
+//Ternary Operator
+$a > 15 ? $z = "True" : $z = "False";
+
+echo $z . "<br>";
