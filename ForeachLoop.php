20eef2a62de1132e7f79b0c0a7dc74b484083a53
sakhawathossensadi <newtonss919@gmail.com>
2022-10-05 16:29:11 +0600

    foreach loop

---
+++ ForeachLoop.php
@@ -0,0 +1,12 @@
+<?php
+
+$ages = [
+    'Sadi' => 27,
+    'Jhon' => 30,
+    'Mike' => 25
+];
+
+//foreach loop
+foreach ($ages as $key => $age) {
+    echo $key . " is " . $age . " years old." . "<br>";
+}
