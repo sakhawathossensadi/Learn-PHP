5daac63a47eedff63bbf18ccff80325250eba220
sakhawathossensadi <newtonss919@gmail.com>
2022-10-06 01:31:01 +0600

    ,  and  in PHP

---
+++ formHandle/Form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Page</title>
+</head>
+
+<body>
+    <form action="FormSubmit.php" method="get">
+        <!-- <form action="FormSubmit.php" method="post"> -->
+        Name : <input type="text" name="name"><br><br>
+        Age : <input type="text" name="age"><br><br>
+        <input type="submit" name="save">
+    </form>
+</body>
+
+</html>
---
+++ formHandle/FormSubmit.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+//$_REQUEST works with both get and post method fo form
+// echo "<pre>";
+// print_r($_REQUEST);
+// echo "</pre>";
