877502773bd511a0a84a1b76d39302d85dbcd357
sakhawathossensadi <newtonss919@gmail.com>
2022-10-05 16:11:16 +0600

    associative array

---
+++ AssociativeArray.php
@@ -0,0 +1,14 @@
+<?php
+
+//associative array
+$age = [
+    'Sadi' => 27,
+    'Jhon' => 30,
+    'Mike' => 25
+];
+
+echo $age['Sadi'] . "<br>";
+echo "<br>";
+echo "<pre>";
+print_r($age);
+echo "</pre>";
